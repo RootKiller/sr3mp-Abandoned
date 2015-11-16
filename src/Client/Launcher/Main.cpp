05f2aac9536bcb97631bf0cb80bb66c246e8beac
Eryk Dwornicki <rootkiller.programmer@gmail.com>
2015-11-16 21:38:37 +0100

    Use of SteamAPI to find game path.

--- src/Client/Launcher/Main.cpp
+++ src/Client/Launcher/Main.cpp
@@ -6,6 +6,11 @@
 #include <Windows.h>
 #include <stdio.h>
 
+#define VERSION_SAFE_STEAM_API_INTERFACES
+#include "SteamAPI/steam_api.h"
+
+#include "Strings.h"
+
 /**
  * Inject DLL into process.
  *
@@ -56,6 +61,39 @@ bool InjectDll(const HANDLE process, const char *const dllPath)
 	return true;
 }
 
+//! Steam api wrapper.
+struct SteamWrapper
+{
+	CSteamAPIContext *context;
+
+	SteamWrapper(void)
+		: context(nullptr)
+	{
+	}
+
+	bool Init(void)
+	{
+		if(! SteamAPI_IsSteamRunning()) {
+			MessageBox(NULL, "To run Saints Row The Third Multiplayer your Steam client must be running.", "Fatal error", MB_ICONERROR);
+			return false;
+		}
+
+		ASSERT(SteamAPI_InitSafe());
+
+		context = new CSteamAPIContext();
+		context->Init();
+		return true;
+	}
+
+	~SteamWrapper(void)
+	{
+		if (context) {
+			delete context;
+			context = nullptr;
+		}
+	}
+};
+
 /**
  * Launcher entry point.
  *
@@ -63,14 +101,25 @@ bool InjectDll(const HANDLE process, const char *const dllPath)
  */
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
-	char GameExePath[MAX_PATH] = { 0 },
-		GamePath[MAX_PATH] = { 0 },
-		Path[MAX_PATH] = { 0 };
+	SteamWrapper steam;
+	if (!steam.Init()) {
+		return 0;
+	}
 
-	// TODO: Registry read /write
+	ISteamApps *const steamApps = steam.context->SteamApps();
+	const AppId_t saintsRowAppid = 55230;
 
-	strcat(GamePath, "G:\\Program Files\\steam\\steamapps\\common\\Saints Row the Third");
-	sprintf(GameExePath, "%s\\SaintsRowTheThird_DX11.exe", GamePath);
+	if (!steamApps->BIsAppInstalled(saintsRowAppid)) {
+		MessageBox(NULL, "To run Saints Row The Third Multiplayer you need to have installed Saints Row The Third game.", "Fatal error", MB_ICONERROR);
+		return 0;
+	}
+
+	char installFolder[MAX_PATH];
+	steamApps->GetAppInstallDir(saintsRowAppid, installFolder, MAX_PATH);
+
+	AFixedString<MAX_PATH> gamePath(installFolder);
+	AFixedString<MAX_PATH> gameExePath(gamePath);
+	gameExePath += "\\SaintsRowTheThird_DX11.exe";
 
 	STARTUPINFO startupInfo = { 0 };
 	PROCESS_INFORMATION processInformation = { 0 };
@@ -78,29 +127,35 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 
 	SetEnvironmentVariable("SteamAppID", "55230");
 
-	if (!CreateProcess(GameExePath, NULL, NULL, NULL, FALSE, CREATE_SUSPENDED, NULL, GamePath, &startupInfo, &processInformation)) {
-		MessageBox(NULL, "Cannot create game process.", "Fatal error", MB_ICONERROR);
-		return false;
+	if (GetFileAttributes(gameExePath) == INVALID_FILE_ATTRIBUTES) {
+		MessageBox(NULL, "Unable to find game .exe file.\n\nGame file: " + gameExePath, "Fatal error", MB_ICONERROR);
+		return 0;
 	}
 
-	GetModuleFileName(NULL, Path, MAX_PATH);
-	for (unsigned int i = strlen(Path); i > 0; --i) {
-		if (Path[i] == '\\') {
-			Path[i] = '\0';
-			break;
-		}
+	if (!CreateProcess(gameExePath, NULL, NULL, NULL, FALSE, CREATE_SUSPENDED, NULL, gamePath, &startupInfo, &processInformation)) {
+		const unsigned lastError = GetLastError();
+		MessageBox(NULL, "Cannot create game process.\n\nGame file: " + gameExePath + "\nDirectory: "+ gamePath +"\n\n(Error code: " + lastError + ")", "Fatal error", MB_ICONERROR);
+		return 0;
 	}
-	strcat(Path, "\\Core.dll");
 
-	if (GetFileAttributes(Path) == INVALID_FILE_ATTRIBUTES) {
+	char cPath[MAX_PATH] = { 0 };
+	GetModuleFileName(NULL, cPath, MAX_PATH);
+	AFixedString<MAX_PATH> path(cPath);
+
+	const unsigned lastPathSeparator = path.FindLast('\\');
+	ASSERT(NotNil(lastPathSeparator));
+	path.CutAt(lastPathSeparator);
+	path += "\\Core.dll";
+
+	if (GetFileAttributes(path) == INVALID_FILE_ATTRIBUTES) {
 		MessageBox(NULL, "Cannot find Core.dll file.", "Error", MB_ICONERROR);
-		return 1;
+		return 0;
 	}
 
-	if (!InjectDll(processInformation.hProcess, Path)) {
+	if (!InjectDll(processInformation.hProcess, path)) {
 		MessageBox(NULL, "Could not inject dll into the game process. Please try launching the game again.", "Fatal error", MB_ICONERROR);
 		TerminateProcess(processInformation.hProcess, 0);
-		return false;
+		return 0;
 	}
 
 	ResumeThread(processInformation.hThread);
